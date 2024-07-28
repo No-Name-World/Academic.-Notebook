b782e7cdf1b29bcabd6bfd17128438ff02ced321
HUDSOn C. <159186112+No-Name-World@users.noreply.github.com>
2024-07-29 02:57:37 +0800

    Create 0005 - Aliases & Unary Relationship.sql

---
+++ Database Management System/SQL (Structured Query Language)/Oracle Apex/Exercise/0005 - Aliases & Unary Relationship.sql
@@ -0,0 +1,17 @@
+-- Question 1 (use tt_script-new.txt)
+-- a) List the age of Clever's father and mother with the following heading (You can assume that Clever’s parent are both non-alive) 
+-- Horse name, Father's Age, Mother's Age
+-- Answer:
+
+
+
+-- b) List Isabelle's siblings' name. (the siblings are from the same parents)
+-- Answer:
+
+
+
+-- c) List Snowy's children's gender in descending order
+-- Answer:
+
+
+###############################################################################################################################################
