bc99f9f1dfd6b323cf964900aa7ac8e5f32a763f
HUDSOn C. <159186112+No-Name-World@users.noreply.github.com>
2024-07-29 02:28:48 +0800

    Create 0002 - Filtering Data.sql

---
+++ Database Management System/SQL (Structured Query Language)/Oracle Apex/Exercise/0002 - Filtering Data.sql
@@ -0,0 +1,25 @@
+-- Question 1
+--  Upload 'useracc.txt' to Oracle APEX --> SQL WORKSHOP --> SQL SCRIPTS. Fix the errors in the script until table useracc is created with five records.
+
+-- Answer:
+
+
+###############################################################################################################################################
+
+-- Question 2 (premiereX.txt)
+-- List all the part details that have their description with the word “washer” in the description
+
+-- Answer:
+
+###############################################################################################################################################
+
+-- Question 3 (premiereX.txt)
+-- List the part number, part description, and on-hand value (OnHand * Price) for each part in item class SG.
+
+-- Answer:
+
+###############################################################################################################################################
+
+
+-- Question 4 (premiereX.txt)
+-- List all the sales representatives whose lastname has first character as 'L' and the fourth character as 'g'. The representatives stay in 'FM' state and have a commission of > 2000.
