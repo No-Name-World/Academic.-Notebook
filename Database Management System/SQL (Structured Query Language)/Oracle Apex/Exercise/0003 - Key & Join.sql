802a120d5a2c8f0f37c57610f2dade8ed5a3a939
HUDSOn C. <159186112+No-Name-World@users.noreply.github.com>
2024-07-29 02:39:37 +0800

    Update 0003 - Key & Join.sql

--- Database Management System/SQL (Structured Query Language)/Oracle Apex/Exercise/0003 - Key & Join.sql
+++ Database Management System/SQL (Structured Query Language)/Oracle Apex/Exercise/0003 - Key & Join.sql
@@ -1,1 +1,16 @@
+-- Question 1 (premiereX.txt)
+-- a) List the total amount the customer Johnson’s Department Store has spent.
+-- Answer:
 
+
+
+-- b) List all the sales representatives’ names (first name and last name) and the number of customers he/she handles. Sort the report in descending order based on the sales representative's name. Display only for sales representatives who have more than two customers.
+-- Answer:
+
+
+
+-- c) List all the active customers’ names. (active customer indicates that the customer has made at least one order)
+-- Answer:
+
+
+###############################################################################################################################################
