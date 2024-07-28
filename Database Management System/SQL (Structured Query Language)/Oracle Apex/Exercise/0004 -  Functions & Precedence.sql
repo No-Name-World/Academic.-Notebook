30b8dafb88e216db3da5cca1b4067e10742372f8
HUDSOn C. <159186112+No-Name-World@users.noreply.github.com>
2024-07-29 02:54:13 +0800

    Create 0004 -  Functions & Precedence.sql

---
+++ Database Management System/SQL (Structured Query Language)/Oracle Apex/Exercise/0004 -  Functions & Precedence.sql
@@ -0,0 +1,16 @@
+-- Question 1 (premiereX.txt)
+-- a) How many orders were placed by the customers in the month of October? 
+-- Answer:
+
+
+
+-- b) List the number and name of all customers who are represented by sales representatives 35 or 65, and that have credit limits of either $10,000 or $15000.
+
+
+
+
+-- c) What is the average number of orders made by customers who live in city Grove. Round up the average value.
+-- Answer:
+
+
+###############################################################################################################################################
