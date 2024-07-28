529845323231a660943eb5bb4c849b5b36d8e13a
HUDSOn C. <159186112+No-Name-World@users.noreply.github.com>
2024-07-29 02:20:26 +0800

    Update 0001 - Introduction to SQL.sql

--- Database Management System/SQL (Structured Query Language)/Oracle Apex/Exercise/0001 - Introduction to SQL.sql
+++ Database Management System/SQL (Structured Query Language)/Oracle Apex/Exercise/0001 - Introduction to SQL.sql
@@ -1,9 +1,9 @@
 -- Exercise 1
-# -- a) Create a table called student with the following attributes: 
+-- a) Create a table called student with the following attributes: 
 -- i. name as VARCHAR2 with the size of 20 characters 
 -- ii. gender as a CHAR of 6 characters 
 -- iii. DOB as a DATE 
--- iv. CGPA as a NUMBER with one decimal 
+-- iv. CGPA is a NUMBER with one decimal 
 
 -- Answer:
 -- Create the student table
@@ -14,6 +14,13 @@ CREATE TABLE student (
     CGPA NUMBER(2,1)
 );
 
+
+
+-- b) Insert the following records: 
+-- i. George, Male, 1 Jan 2001, 3.2 
+-- ii. Jane, Female, 21 Dec 1999, 2.6 
+
+-- Answer:
 -- Insert the records
 INSERT INTO student (name, gender, DOB, CGPA) 
 VALUES ('George', 'Male', TO_DATE('01-Jan-2001', 'DD-Mon-YYYY'), 3.2);
@@ -21,12 +28,20 @@ VALUES ('George', 'Male', TO_DATE('01-Jan-2001', 'DD-Mon-YYYY'), 3.2);
 INSERT INTO student (name, gender, DOB, CGPA) 
 VALUES ('Jane', 'Female', TO_DATE('21-Dec-1999', 'DD-Mon-YYYY'), 2.6);
 
--- View the contents of the table
+
+
+-- c) View the contents of the table
+-- Answer:
 SELECT * FROM student;
 
--- Delete a record from the table
+
+
+-- d)) Delete a record from the table
+-- Answer:
 -- For example, to delete Jane's record:
 DELETE FROM student WHERE name = 'Jane';
 
--- Drop the table
+
+
+-- e) Drop the table
 DROP TABLE student;
