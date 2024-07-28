da1dd75f16851c0a653802a90adbc615388a255a
HUDSOn C. <159186112+No-Name-World@users.noreply.github.com>
2024-07-29 03:07:02 +0800

    Create 0007 - Subqueries.sql

---
+++ Database Management System/SQL (Structured Query Language)/Oracle Apex/Exercise/0007 - Subqueries.sql
@@ -0,0 +1,21 @@
+-- Question 1 (use tt_script-new.txt)
+-- a) Write an SQL statement with no join conditions. Which horse (mare's name) has given birth to the most children?
+
+-- Answer:
+
+
+
+-- b) List Sky's sibling's age.(the siblings are from the same parents)
+-- Answer:
+
+
+
+-- c) List Snowy's youngest child
+-- Answer:
+
+
+
+-- d) List the age of Clever's father and mother with the following heading (You can assume that Clever’s parent are alive or non-alive):
+-- Horse name, Father's Age, Mother's Age
+-- Answer:
+###############################################################################################################################################
